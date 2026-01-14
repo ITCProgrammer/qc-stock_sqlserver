87db854d21ddd67c66665bfd5ba80a29c20f7006
Muhajir <hajir.biz@gmail.com>
2026-01-14 16:08:12 +0700

    refactor: data stock now

--- controllers/StockNowClass.php
+++ controllers/StockNowClass.php
@@ -2,36 +2,75 @@
 class StockNow extends Database
 {
     public $conn;
+    private $connSqlSrv;
+
     public function __construct()
     {
         // Panggil parent constructor jika ada, lalu set koneksi
         $this->conn = $this->connectMySQLi();
+        $this->connSqlSrv = $this->connectSqlServer();
     }
 
     public function LihatDataNow()
     {
-        $datastoknow = array();
-        // Pastikan koneksi sudah benar
-        if (!$this->conn) {
-            return $datastoknow;
+        // $datastoknow = array();
+        // // Pastikan koneksi sudah benar
+        // if (!$this->conn) {
+        //     return $datastoknow;
+        // }
+        // $data = $this->conn->query("SELECT * FROM tbl_master_barang where ITEMTYPECODE ='PCK' ORDER BY id ASC");
+        // if ($data) {
+        //     while ($x = mysqli_fetch_assoc($data)) {
+        //         $datastoknow[] = $x;
+        //     }
+        // }
+        // return $datastoknow;
+
+        $sql = "SELECT * FROM invqc.tbl_master_barang where ITEMTYPECODE ='PCK' ORDER BY id ASC";
+        $query = sqlsrv_query($this->connSqlSrv, $sql);
+        if ($query === false) {
+            die(print_r(sqlsrv_errors(), true));
         }
-        $data = $this->conn->query("SELECT * FROM tbl_master_barang where ITEMTYPECODE ='PCK' ORDER BY id ASC");
-        if ($data) {
-            while ($x = mysqli_fetch_assoc($data)) {
-                $datastoknow[] = $x;
-            }
+
+        $result = [];
+
+        while ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
+            $result[] = $row;
         }
-        return $datastoknow;
+
+        return $result;
     }
 
     public function UpdateMinMina($id, $min, $mina)
     {
-        if (!$this->conn) return false;
-        $id = intval($id);
-        $min = intval($min);
-        $mina = intval($mina);
-        $sql = "UPDATE tbl_master_barang SET `MIN` = '$min', `MINA` = '$mina' WHERE id = '$id'";
-        return $this->conn->query($sql);
+        // if (!$this->conn) return false;
+        // $id = intval($id);
+        // $min = intval($min);
+        // $mina = intval($mina);
+        // $sql = "UPDATE tbl_master_barang SET `MIN` = '$min', `MINA` = '$mina' WHERE id = '$id'";
+        // return $this->conn->query($sql);
+
+        $id   = (int) $id;
+        $min  = (int) $min;
+        $mina = (int) $mina;
+
+        $sql = "
+        UPDATE invqc.tbl_master_barang
+        SET
+            [MIN]  = ?,
+            [MINA] = ?
+        WHERE id = ?
+    ";
+
+        $params = [$min, $mina, $id];
+
+        $query = sqlsrv_query($this->connSqlSrv, $sql, $params);
+
+        if ($query === false) {
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+        return true;
     }
 
 
--- controllers/barangNowClass.php
+++ controllers/barangNowClass.php
@@ -1,15 +1,34 @@
 <?php
 class BarangNow extends Database
 {
+    private $conn;
+    private $connSqlSrv;
+
     public function __construct()
     {
         $this->conn = $this->connectMySQLi();
+        $this->connSqlSrv = $this->connectSQLServer();
     }
 
     public function getBarangList()
     {
-        $query  = "SELECT id, DESCRIPTION FROM tbl_master_barang";
-        $result = $this->conn->query($query);
+        // $query  = "SELECT id, DESCRIPTION FROM tbl_master_barang";
+        // $result = $this->conn->query($query);
+
+        // return $result;
+
+        $sql = "SELECT id, DESCRIPTION FROM invqc.tbl_master_barang";
+
+        $stmt = sqlsrv_query($this->connSqlSrv, $sql);
+
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+        $result = [];
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            $result[] = $row;
+        }
 
         return $result;
     }
@@ -17,12 +36,15 @@ class BarangNow extends Database
     public function renderBarangSelect($selectedBarangID = null)
     {
         $barangList = $this->getBarangList();
-        echo '<select id="nama_barang" name="nama_barang" class="form-control" required>';
-        echo '<option value="" selected>Pilih Barang</option>';
 
-        while ($row = $barangList->fetch_assoc()) {
+        echo '<select id="nama_barang" name="nama_barang" class="form-control" required>';
+        echo '<option value="">Pilih Barang</option>';
+
+        foreach ($barangList as $row) {
             $selected = ($selectedBarangID == $row['id']) ? 'selected' : '';
-            echo '<option value="' . $row['id'] . '" ' . $selected . '>' . htmlspecialchars($row['DESCRIPTION']) . '</option>';
+            echo '<option value="' . $row['id'] . '" ' . $selected . '>'
+                . htmlspecialchars($row['DESCRIPTION'], ENT_QUOTES, 'UTF-8')
+                . '</option>';
         }
 
         echo '</select>';
